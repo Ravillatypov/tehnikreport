e8aa2f44017f9650fb65fd85bee6576bbeb51826
Ravil Latypov <ravillatypov12@gmail.com>
2018-03-07 16:54:43 +0300

    начал разделение на файлы

--- db.go
+++ db.go
@@ -2,36 +2,14 @@ package tehnikreport
 
 import (
 	"database/sql"
-	"log"
-	"strings"
-
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"gopkg.in/telegram-bot-api.v4"
 )
-
-// Report хранит введенные данные техником
-// после заполнения необходимых полей, отчет отправляется координатору
-// струкура удаляется
-type Report struct {
-	Id, BSO   int32      // номер заявки и номер БСО
-	Comment   string     // комментарии техника по заявке, здесь же можно указать услуги
-	Status    bool       // заявка выполнена или нет
-	Services  []Service  // перечень выполненных работ
-	Amount    float32    // сумма оказанных услуг
-	Materials []Material // затраченные материалы
-}
-
-// id материала и количество
-type Material struct {
-	Id, Count int
-}
-
-// какая работа была выполнена
-type Service struct {
-	Type, Job int
-}
 
 // тип для взаимодействия с базой СУЗа
 // перед началом должен быть инициализирован
@@ -43,89 +21,7 @@ type Db struct {
 	sTiketsByUserid *sql.Stmt // выборка незакрытых заявок пользователя по id
 }
 
-var (
-	ServiceTypes = []string{
-		"Софт",
-		"Кабель",
-		"Телевидение",
-		"Роутер",
-	}
-	ServiceList = [][]string{
-		{
-			"Оптимизация ОС",
-			"Восстановление ОС",
-			"Сканирование на вирусы",
-			"Удаление вирусов",
-			"Установка антивируса",
-			"Чистка реестра",
-			"Настройка PPPoE соединения",
-			" Регистрация/восстановление УЗ",
-			"Установка базового пакета ПО",
-			"Установка офисного пакета",
-			"Установка драйверов 1-10шт",
-		},
-		{
-			"Укладка кабеля открытым способом",
-			"Укладка кабеля закрытым способом",
-			"Скочлок соединение",
-			"Обжимка коннектора",
-			"Установка F-разъема",
-			"Установка ТВ-делителя",
-			"Установка соединительной бочки",
-			"Установка ТВ-штекера",
-			"Строительно-монтажные работы",
-		},
-		{
-			"Автоматическая настройка ТВ",
-			"Ручная настройка каналов (1-60шт)",
-			"Настройка SMART",
-			"Обновление прошивки",
-			"Сброс до заводских настроек",
-			"Настройка цветности",
-			"Настройка звука",
-		},
-		{
-			"Настройка PPPoE",
-			"Настройка канала вещания WiFi",
-			"Настройка шифрования",
-			"Обновление прошивки",
-		},
-	}
-	MaterialList = []string{
-		"",
-		"Розетка",
-		"Скотчлок",
-		"Бочка",
-		"RJ-45",
-		"UTP-5",
-		"RG-6",
-		"ТВ-штеккер",
-		"F-разъём",
-		"Делитель ТВ",
-	}
-)
-
-// переменная хранит значения введенные техником, но еще не отправленные координатору
-// после отправки отчета коорднатору, удаляется элемент из карты
-// ключем является chat_id
-var Reports = make(map[uint64]Report)
-
-// кнопки выбора типа выполненных работ
-var ServiceTypeKeyb = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData(ServiceTypes[0], "0"),
-		tgbotapi.NewInlineKeyboardButtonData(ServiceTypes[1], "1"),
-	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData(ServiceTypes[2], "2"),
-		tgbotapi.NewInlineKeyboardButtonData(ServiceTypes[3], "3"),
-	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("все введено", "remove"),
-	),
-)
-
-func initialize(dbconfig string) (*Db, error) {
+func Initialize(dbconfig string) (*Db, error) {
 	suz, err := sql.Open("mysql", dbconfig)
 	defer suz.Close()
 	if err != nil {
@@ -225,5 +121,17 @@ func Help(b *tgbotapi.BotAPI, u *tgbotapi.Update) {
 
 // парсинг сообщения для сбора нужной информации по отчету
 func ParseReport(b *tgbotapi.BotAPI, u *tgbotapi.Update) {
+	if u.CallbackQuery != nil {
+		var calbackdata = u.CallbackQuery.Data
+		if strings.HasPrefix(calbackdata, "report") {
+			b.DeleteMessage(tgbotapi.DeleteMessageConfig{ChatID: u.CallbackQuery.Message.Chat.ID, MessageID: u.CallbackQuery.Message.MessageID})
+			tikid, err := strconv.Atoi(strings.Split(calbackdata, "report")[1])
+			if err != nil {
+				log.Println(err.Error())
+				return
+			}
+			Reports[u.CallbackQuery.Message.Chat.ID] = Report{Id: tikid}
 
+		}
+	}
 }
