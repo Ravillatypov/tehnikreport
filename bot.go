b8e0cfa2295b646bce334769f90c6631a5a2e896
Ravil Latypov <ravillatypov12@gmail.com>
2018-03-10 14:46:43 +0300

    описал структуру программы, добавил тип chatbot для объединения нужных данных

--- bot.go
+++ bot.go
@@ -6,6 +6,13 @@ import (
 	"gopkg.in/telegram-bot-api.v4"
 )
 
+// chatbot тип для хранения всего что нужно в одном месте
+type chatbot struct {
+	db    *Db
+	state *ChatState
+	bot   *tgbotapi.BotAPI
+}
+
 // переменная хранит значения введенные техником, но еще не отправленные координатору
 // после отправки отчета коорднатору, удаляется элемент из карты
 // ключем является chat_id
@@ -24,19 +31,19 @@ var ServiceTypeKeyb = tgbotapi.NewInlineKeyboardMarkup(
 	),
 )
 
-// функция отправляет сообщение-инструкцию как пользоваться ботом
-func Help(b *tgbotapi.BotAPI, u *tgbotapi.Update) {
+// Help функция отправляет сообщение-инструкцию как пользоваться ботом
+func (ch *chatbot) Help(u *tgbotapi.Update) {
 	msg := tgbotapi.NewMessage(u.Message.Chat.ID, `Бот предназначен для сбора отчетов о выполненных работах.
 		для отправки отчетов необходимо авторизваться с помощью комманды /login
 		после авторизации можно будет смотреть свои незакрытые заявки коммандой /tiket
 		под каждой заявкой есть кнопочка, с помощю которой можно отправить отчет`)
-	b.Send(msg)
+	ch.bot.Send(msg)
 	video := tgbotapi.NewVideoUpload(u.Message.Chat.ID, "help.avi")
 	video.Caption = "пример как пользоваться"
-	b.Send(video)
+	ch.bot.Send(video)
 }
 
-// парсинг сообщения для сбора нужной информации по отчету
+// ParseReport парсинг сообщения для сбора нужной информации по отчету
 func ParseReport(b *tgbotapi.BotAPI, u *tgbotapi.Update) {
 	if u.CallbackQuery != nil {
 		var calbackdata = u.CallbackQuery.Data
@@ -50,3 +57,24 @@ func ParseReport(b *tgbotapi.BotAPI, u *tgbotapi.Update) {
 		}
 	}
 }
+
+// ParseUpdate
+func (ch *chatbot) ParseUpdate(u *tgbotapi.Update) {}
+
+// NewReport
+func (ch *chatbot) NewReport(cal *tgbotapi.CallbackQuery) {}
+
+// Status
+func (ch *chatbot) Status(cal *tgbotapi.CallbackQuery) {}
+
+// Services
+func (ch *chatbot) Services(cal *tgbotapi.CallbackQuery) {}
+
+// Materials
+func (ch *chatbot) Materials(cal *tgbotapi.CallbackQuery) {}
+
+// Bso
+func (ch *chatbot) Bso(u *tgbotapi.Update) {}
+
+// Amount
+func (ch *chatbot) Amount(u *tgbotapi.Update) {}
