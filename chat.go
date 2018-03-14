fce8ebf44296a548227972a3ea520801352de060
Ravil Latypov <ravillatypov12@gmail.com>
2018-03-14 21:40:54 +0300

    fix: Login, Super, ChatState авторизация проверкой пароля

--- chat.go
+++ chat.go
@@ -8,6 +8,78 @@ import (
 	"gopkg.in/telegram-bot-api.v4"
 )
 
+// String map[int64]string
+type String struct {
+	sync.RWMutex
+	s map[int64]string
+}
+
+func (s *String) set(id int64, val string) {
+	s.Lock()
+	defer s.Unlock()
+	s.s[id] = val
+}
+
+func (s *String) del(id int64) {
+	s.Lock()
+	defer s.Unlock()
+	delete(s.s, id)
+}
+
+func (s *String) get(id int64) string {
+	s.RLock()
+	defer s.RUnlock()
+	return s.s[id]
+}
+
+// Uint16 map[int64]uint16
+type Uint16 struct {
+	sync.RWMutex
+	s map[int64]uint16
+}
+
+func (s *Uint16) set(id int64, val uint16) {
+	s.Lock()
+	defer s.Unlock()
+	s.s[id] = val
+}
+
+func (s *Uint16) del(id int64) {
+	s.Lock()
+	defer s.Unlock()
+	delete(s.s, id)
+}
+
+func (s *Uint16) get(id int64) uint16 {
+	s.RLock()
+	defer s.RUnlock()
+	return s.s[id]
+}
+
+// Reportmap map[int64]*Report
+type Reportmap struct {
+	sync.RWMutex
+	s map[int64]*Report
+}
+
+func (s *Reportmap) set(id int64, val *Report) {
+	s.Lock()
+	defer s.Unlock()
+	s.s[id] = val
+}
+
+func (s *Reportmap) del(id int64) {
+	s.Lock()
+	defer s.Unlock()
+	delete(s.s, id)
+}
+
+func (s *Reportmap) get(id int64) *Report {
+	s.RLock()
+	defer s.RUnlock()
+	return s.s[id]
+}
+
 // Report хранит введенные данные техником
 // после заполнения необходимых полей, отчет отправляется координатору,
 // в группу и пользователям авторизованных как super
@@ -102,62 +174,39 @@ var (
 	выполненные работы: %s
 	комментарии: %s
 	`
+	// ForCoordirantors шаблон письма для координатора
+	ForCoordirantors = `Техник: %s
+%s`
 )
 
 // ChatState тип для хранения состояния чата
 type ChatState struct {
-	sync.RWMutex                   // нужна синхронизация для мапов
-	reports      map[int64]*Report // сохраняем для формировании  отчета
-	action       map[int64]string  // что ждем от пользователя, какую инфу
-	users        map[int64]uint16  // сопоставление chat_id и внутеннего id
-	super        []int64           // чаты руководителей и коорднаторов
-}
-
-// GetAction используется для получения action
-func (c *ChatState) GetAction(u int64) string {
-	log.Println("GetAction", u)
-	c.RLock()
-	defer c.RUnlock()
-	return c.action[u]
-}
-
-// SetAction задает следующее действие для чата
-func (c *ChatState) SetAction(u int64, ac string) {
-	log.Println("SetAction", u, ac)
-	c.Lock()
-	defer c.Unlock()
-	c.action[u] = ac
-}
-
-// GetReport формирует отчет
-func (c *ChatState) GetReport(u int64) string {
-	log.Println("GetReport", u)
-	c.RLock()
-	defer c.RUnlock()
-	return c.reports[u].MakeReport()
+	reports Reportmap // сохраняем для формировании  отчета
+	action  String    // что ждем от пользователя, какую инфу
+	phone   String    // номер телефона
+	name    String    // ФИО в системе
+	users   Uint16    // сопоставление chat_id и внутеннего id
+	super   []int64   // чаты руководителей и коорднаторов
 }
 
 // AddService добавляет выполненную работу
 func (c *ChatState) AddService(u int64, s *Service) {
 	log.Println("AddService", u, *s)
-	c.Lock()
-	defer c.Unlock()
-	for _, v := range c.reports[u].Services {
+	serv := c.reports.get(u).Services
+	for _, v := range serv {
 		if v == *s {
 			return
 		}
 	}
-	c.reports[u].Services = append(c.reports[u].Services, (*s))
+	c.reports.get(u).Services = append(serv, (*s))
 }
 
 // AddMaterials добавляет материал
 func (c *ChatState) AddMaterials(u int64, m *Material) {
 	log.Println("AddMaterials", u, *m)
-	c.Lock()
-	defer c.Unlock()
 	newmat := make([]Material, 0)
 	newmat = append(newmat, *m)
-	for _, v := range c.reports[u].Materials {
+	for _, v := range c.reports.get(u).Materials {
 		if v == *m {
 			return
 		}
@@ -166,39 +215,21 @@ func (c *ChatState) AddMaterials(u int64, m *Material) {
 		}
 		newmat = append(newmat, v)
 	}
-	c.reports[u].Materials = newmat
+	c.reports.get(u).Materials = newmat
 }
 
 // SetMaterialsCount указывает количество материала
 func (c *ChatState) SetMaterialsCount(u int64, count uint8) {
 	log.Println("SetMaterialCount", u, count)
-	c.Lock()
-	defer c.Unlock()
 	mat := make([]Material, 0)
-	for _, v := range c.reports[u].Materials {
+	for _, v := range c.reports.get(u).Materials {
 		if v.Count == 0 {
 			v.Count = count
 			log.Printf("SetCount %v", mat)
 		}
 		mat = append(mat, v)
 	}
-	c.reports[u].Materials = mat
-}
-
-// SetStatus устанавливает статус выполнения заявки
-func (c *ChatState) SetStatus(u int64, s bool) {
-	log.Println("SetStatus", u, s)
-	c.Lock()
-	defer c.Unlock()
-	c.reports[u].Status = s
-}
-
-// GetStatus получает статус выполнения заявки
-func (c *ChatState) GetStatus(u int64) bool {
-	log.Println("GetStatus", u)
-	c.RLock()
-	defer c.RUnlock()
-	return c.reports[u].Status
+	c.reports.get(u).Materials = mat
 }
 
 // MakeReport создает отчет координатору
@@ -252,27 +283,9 @@ func (m *Material) Print() string {
 	return MaterialList[m.ID] + fmt.Sprintf(" %d шт.;\n", m.Count)
 }
 
-// AddUser добавляет нового пользователя
-func (c *ChatState) AddUser(chatid int64, uid uint16) {
-	log.Println("AddUser", chatid, uid)
-	c.Lock()
-	defer c.Unlock()
-	c.users[chatid] = uid
-}
-
-// GetUserID получаем id пользователя
-func (c *ChatState) GetUserID(chatid int64) uint16 {
-	log.Println("GetUserID", chatid)
-	c.RLock()
-	defer c.RUnlock()
-	return c.users[chatid]
-}
-
 // IsCable были ли кабельные работы
 func (c *ChatState) IsCable(chatid int64) bool {
-	c.RLock()
-	defer c.RUnlock()
-	for _, s := range c.reports[chatid].Services {
+	for _, s := range c.reports.get(chatid).Services {
 		if s.Type == 1 {
 			return true
 		}
@@ -280,68 +293,14 @@ func (c *ChatState) IsCable(chatid int64) bool {
 	return false
 }
 
-// AddReport создаем новый отчет для данного чата
-func (c *ChatState) AddReport(chatid int64, reportid uint32) {
-	c.Lock()
-	defer c.Unlock()
-	c.reports[chatid] = &Report{ID: reportid}
-}
-
-// SetBso установливаем номер БСО
-func (c *ChatState) SetBso(chatid int64, bso uint32) {
-	c.Lock()
-	defer c.Unlock()
-	c.reports[chatid].BSO = bso
-}
-
-// SetAmount установливаем сумму услуг
-func (c *ChatState) SetAmount(chatid int64, amount uint16) {
-	c.Lock()
-	defer c.Unlock()
-	c.reports[chatid].Amount = amount
-}
-
 // Clear очищает отчет, состояние чата
 func (c *ChatState) Clear(chatid int64) {
-	c.Lock()
-	defer c.Unlock()
-	delete(c.reports, chatid)
-	delete(c.action, chatid)
-}
-
-// SetComment меняем коммент
-func (c *ChatState) SetComment(chatid int64, comment string) {
-	c.Lock()
-	defer c.Unlock()
-	c.reports[chatid].Comment = comment
-}
-
-// SetDopServices меняем коммент
-func (c *ChatState) SetDopServices(chatid int64, services string) {
-	c.Lock()
-	defer c.Unlock()
-	c.reports[chatid].DopServices = services
-}
-
-// MakeReport создает отчет для указанного чата
-func (c *ChatState) MakeReport(chatid int64) string {
-	c.RLock()
-	defer c.RUnlock()
-	return c.reports[chatid].MakeReport()
-}
-
-// LoadUsers меняем коммент
-func (c *ChatState) LoadUsers(uids *map[int64]uint16) {
-	log.Println("ChatState LoadUsers", *uids)
-	c.Lock()
-	defer c.Unlock()
-	c.users = (*uids)
+	c.reports.del(chatid)
+	c.action.del(chatid)
 }
 
 // AddSuper меняем коммент
 func (c *ChatState) AddSuper(chatid int64) {
-	c.Lock()
-	defer c.Unlock()
 	s := make([]int64, 0)
 	for _, v := range c.super {
 		if v == chatid {
--- db.go
+++ db.go
@@ -1,6 +1,7 @@
 package tehnikreport
 
 import (
+	"crypto/sha512"
 	"database/sql"
 	"log"
 	"strings"
@@ -13,7 +14,6 @@ import (
 type Db struct {
 	mysql           *sql.DB   // структура базы
 	sUserByPhone    *sql.Stmt // найти пользователя по  номеру телефона
-	sUserByChatid   *sql.Stmt // найти пользователя по chat_id
 	uUserChatid     *sql.Stmt // изменение chat_id пользователя
 	sTiketsByUserid *sql.Stmt // выборка незакрытых заявок пользователя по id
 }
@@ -30,11 +30,7 @@ func Initialize(dbconfig string) (*Db, error) {
 	if err != nil {
 		return &Db{}, err
 	}
-	suphon, err := suz.Prepare(`SELECT id,status FROM mms_adm_users WHERE phone_number LIKE ? LIMIT 1`)
-	if err != nil {
-		return &Db{}, err
-	}
-	suchat, err := suz.Prepare(`SELECT id,status FROM mms_adm_users WHERE chat_id LIKE ? LIMIT 1`)
+	suphon, err := suz.Prepare(`SELECT id,status,password,fio FROM mms_adm_users WHERE phone_number LIKE ? LIMIT 1`)
 	if err != nil {
 		return &Db{}, err
 	}
@@ -46,71 +42,84 @@ func Initialize(dbconfig string) (*Db, error) {
 	if err != nil {
 		return &Db{}, err
 	}
-	return &Db{mysql: suz, sUserByChatid: suchat, sUserByPhone: suphon, uUserChatid: uuchat, sTiketsByUserid: stbid}, nil
+	return &Db{mysql: suz, sUserByPhone: suphon, uUserChatid: uuchat, sTiketsByUserid: stbid}, nil
 }
 
 // Login функция для авторизации техников
-// авторизация по номеру телефона(без отправки смс)
-func (d *Db) Login(phone string, ChatID int64) (bool, uint16) {
-	log.Println("DB Login", phone, ChatID)
+func (d *Db) Login(phone string, pass string, ChatID int64) (res bool, id uint16, fio string) {
+	log.Println("DB Login", phone, pass, ChatID)
 	ln := strings.Count(phone, "")
 	if ln < 11 {
-		return false, 0
+		return
 	}
 	phone = phone[ln-11:]
-	status := 55
-	var id uint16
+	var (
+		passwd string
+		status int
+	)
+	log.Println(phone)
+	hash := sha512.New()
+	hash.Write([]byte(pass))
+	pass = string(hash.Sum(nil))
+	log.Println(pass)
 	rows, err := d.sUserByPhone.Query("%" + phone)
-	defer rows.Close()
 	if err != nil {
 		log.Println(err.Error())
-		return false, 0
+		return
 	}
+	defer rows.Close()
 	rows.Next()
-	err = rows.Scan(&id, &status)
+	err = rows.Scan(&id, &status, &passwd, &fio)
 	if err != nil {
 		log.Println("DB Login", err.Error())
-		return false, 0
+		return
 	}
 	log.Printf("id = %d, status = %d", id, status)
-	if status == 0 && id != 0 {
+	if status == 0 && id != 0 && pass == passwd {
+		res = true
 		rs, err := d.uUserChatid.Exec(ChatID, id)
 		if err != nil {
 			log.Println(err.Error())
-			return true, id
+			return
 		}
 		affect, err := rs.RowsAffected()
 		log.Printf("updated %d rows", affect)
-		return true, id
+		return
 	}
-	return false, 0
+	return
 }
 
 // SuperLogin функция для авторизации техников
 // авторизация по номеру телефона(без отправки смс)
-func (d *Db) SuperLogin(phone string, ChatID int64) (bool, uint16) {
-	log.Println("DB Login", phone, ChatID)
+func (d *Db) SuperLogin(phone, pass string, ChatID int64) (bool, uint16) {
+	log.Println("DB SuperLogin", phone, ChatID)
 	ln := strings.Count(phone, "")
 	if ln < 11 {
 		return false, 0
 	}
 	phone = phone[ln-11:]
-	status := 55
-	var id uint16
-	rows, err := d.mysql.Query("SELECT id,status FROM mms_adm_users WHERE gid != 12 AND phone_number LIKE ? LIMIT 1", "%"+phone)
-	defer rows.Close()
+	var (
+		id     uint16
+		passwd string
+		status int
+	)
+	hash := sha512.New()
+	hash.Write([]byte(pass))
+	pass = string(hash.Sum(nil))
+	rows, err := d.mysql.Query("SELECT id,status,password FROM mms_adm_users WHERE gid != 12 AND phone_number LIKE ? LIMIT 1", "%"+phone)
 	if err != nil {
 		log.Println(err.Error())
 		return false, 0
 	}
+	defer rows.Close()
 	rows.Next()
-	err = rows.Scan(&id, &status)
+	err = rows.Scan(&id, &status, &passwd)
 	if err != nil {
 		log.Println("DB Login", err.Error())
 		return false, 0
 	}
 	log.Printf("id = %d, status = %d", id, status)
-	if status == 0 && id != 0 {
+	if status == 0 && id != 0 && pass == passwd {
 		rs, err := d.uUserChatid.Exec(ChatID, id)
 		if err != nil {
 			log.Println(err.Error())
@@ -129,10 +138,11 @@ func (d *Db) LoadTikets(uid uint16) []Tiket {
 	t := make([]Tiket, 0)
 	if uid != 0 {
 		rows, err := d.sTiketsByUserid.Query(uid)
-		defer rows.Close()
 		if err != nil {
 			log.Println("Db LoadTikets", uid, err.Error())
+			return t
 		}
+		defer rows.Close()
 		for rows.Next() {
 			var (
 				tiketid         uint32
@@ -152,23 +162,27 @@ func (d *Db) LoadTikets(uid uint16) []Tiket {
 }
 
 // LoadUsers загружает с базы авторизованные учетки
-func (d *Db) LoadUsers() *map[int64]uint16 {
+func (d *Db) LoadUsers() (map[int64]uint16, map[int64]string) {
 	log.Println("Db LoadUsers")
-	res := make(map[int64]uint16)
-	var chatid int64
-	var uid uint16
-	rows, err := d.mysql.Query(`SELECT id,chat_id FROM mms_adm_users WHERE chat_id != 0 AND status = 0`)
-	defer rows.Close()
+	ids := make(map[int64]uint16)
+	names := make(map[int64]string)
+	var (
+		chatid int64
+		uid    uint16
+		fio    string
+	)
+	rows, err := d.mysql.Query(`SELECT id,chat_id,fio FROM mms_adm_users WHERE chat_id != 0 AND status = 0 AND gid = 12`)
 	if err != nil {
 		log.Println("Db LoadUsers", err.Error())
 	}
+	defer rows.Close()
 	for rows.Next() {
-		rows.Scan(&uid, &chatid)
-		res[chatid] = uid
-		log.Println("Db LoadUsers", uid, chatid)
+		rows.Scan(&uid, &chatid, &fio)
+		ids[chatid] = uid
+		names[chatid] = fio
+		log.Println("Db LoadUsers", uid, chatid, fio)
 	}
-	log.Println("Db LoadUsers", res)
-	return &res
+	return ids, names
 }
 
 // LoadSupers загружает с базы авторизованные учетки
@@ -176,13 +190,15 @@ func (d *Db) LoadSupers() []int64 {
 	res := make([]int64, 0)
 	var chatid int64
 	rows, err := d.mysql.Query(`SELECT chat_id FROM mms_adm_users WHERE chat_id != 0 AND status = 0 AND gid != 12`)
-	defer rows.Close()
 	if err != nil {
 		log.Println("Db LoadSupers", err.Error())
 	}
+	defer rows.Close()
 	for rows.Next() {
 		rows.Scan(&chatid)
 		res = append(res, chatid)
+		log.Println("LOadSuper: ", chatid)
 	}
+	log.Println("LOadSuper: ", res)
 	return res
 }
